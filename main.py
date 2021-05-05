6f0c69e981751117550502c825ce07787020c8c1
shettyrahul17 <83722365+shettyrahul17@users.noreply.github.com>
2021-05-06 00:22:38 +0530

    Revert "Rahul changed"

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import parse
 import token
 
 
-<<<<<<< HEAD
-=======
 #nithin
->>>>>>> f379c722a4f4805cee94246e3910f464267475ff
 # To take Multiline input from user.
 print("Enter/Paste your code.Ctrl-Z ( windows ) to save it.")
 contents = ""
